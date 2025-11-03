6fc42f24f52981f6d0b248b886450ccadb33b3fd
pixelpxed <pixel.pxed@gmail.com>
2025-11-03 09:27:19 +0700

    feat: optimization

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,8 @@ const display = Tomorrow({
 
 const Landing = () => {
   const eventName = "Jark-yao 144";
-  const targetTime = new Date("2026-02-20T21:00:00+07:00").getTime();
+  const enrolledTime = new Date("2020-06-29T07:30:00+07:00").getTime();
+  const jarkyaoTime = new Date("2026-02-20T21:00:00+07:00").getTime();
   const [timeLeft, setTimeLeft] = useState({
     info: {
       difference: 0,
@@ -29,7 +30,7 @@ const Landing = () => {
   useEffect(() => {
     setInterval(() => {
       const now = new Date().getTime();
-      const difference = targetTime - now;
+      const difference = jarkyaoTime - now;
 
       const milliSecToSec = 1000 * 60;
 
@@ -56,73 +57,80 @@ const Landing = () => {
       className={`${body.className} flex flex-col min-h-screen items-center 
           justify-center font-sans text-zinc-900 dark:text-zinc-100
           bg-zinc-200 dark:bg-zinc-900`}
-    >
-      {/* Header (Title) */}
-      <p className={`fixed top-8 text-center opacity-50`}>{eventName}</p>
-      {/* Countdown */}
-      <div
-        className={`relative ${display.className} flex items-center flex-col sm:flex-row gap-0 sm:gap-6 -translate-y-0.5`}
       >
-        {/* Number Fading Effect */}
+        {/* Header (Title) */}
+        <p className={`fixed top-8 text-center opacity-50`}>{eventName}</p>
+        {/* Countdown */}
         <div
-          className="absolute top-0 w-full h-2.5 z-10 
+          className={`relative ${display.className} flex items-center flex-col sm:flex-row gap-0 sm:gap-6 -translate-y-0.5`}
+        >
+          {/* Number Fading Effect */}
+          <div
+            className="absolute top-0 w-full h-2.5 z-10 
           bg-linear-to-t from-transparent to-zinc-200 dark:to-zinc-900"
-        />
-        <div
-          className="absolute bottom-0 w-full h-2.5 z-10 
+          />
+          <div
+            className="absolute bottom-0 w-full h-2.5 z-10 
           bg-linear-to-b from-transparent to-zinc-200 dark:to-zinc-900"
-        />
+          />
 
-        {/* Day Counter */}
-        <div className="flex text-6xl leading-none">
-          <span className="translate-y-0.5">T</span>
-          <span
-            className={
-              !(timeLeft.info.difference > 0) ? "" : "-translate-y-0.5"
-            }
-          >
-            {!(timeLeft.info.difference > 0) ? "+" : "-"}
-          </span>
-          <AnimateDigit value={Math.floor(timeLeft.calculation.day / 100)} />
-          <AnimateDigit value={Math.floor(timeLeft.calculation.day / 100)} />
-          <AnimateDigit value={Math.floor(timeLeft.calculation.day % 10)} />
+          {/* Day Counter */}
+          <div className="flex text-6xl leading-none">
+            <span className="translate-y-0.5">T</span>
+            <span
+              className={
+                !(timeLeft.info.difference > 0) ? "" : "-translate-y-0.5"
+              }
+            >
+              {!(timeLeft.info.difference > 0) ? "+" : "-"}
+            </span>
+            <AnimateDigit value={Math.floor(timeLeft.calculation.day / 100)} />
+            <AnimateDigit value={Math.floor(timeLeft.calculation.day / 100)} />
+            <AnimateDigit value={Math.floor(timeLeft.calculation.day % 10)} />
+          </div>
+
+          {/* Time Counter */}
+          <div className="flex items-center text-6xl leading-none">
+            <AnimateDigit value={Math.floor(timeLeft.calculation.hour / 10)} />
+            <AnimateDigit value={timeLeft.calculation.hour % 10} />
+            <span className="-translate-y-1">:</span>
+            <AnimateDigit
+              value={Math.floor(timeLeft.calculation.minute / 10)}
+            />
+            <AnimateDigit value={timeLeft.calculation.minute % 10} />
+            <span className="-translate-y-1">:</span>
+            <AnimateDigit
+              value={Math.floor(timeLeft.calculation.second / 10)}
+            />
+            <AnimateDigit value={timeLeft.calculation.second % 10} />
+          </div>
         </div>
 
-        {/* Time Counter */}
-        <div className="flex items-center text-6xl leading-none">
-          <AnimateDigit value={Math.floor(timeLeft.calculation.hour / 10)} />
-          <AnimateDigit value={timeLeft.calculation.hour % 10} />
-          <span className="-translate-y-1">:</span>
-          <AnimateDigit value={Math.floor(timeLeft.calculation.minute / 10)} />
-          <AnimateDigit value={timeLeft.calculation.minute % 10} />
-          <span className="-translate-y-1">:</span>
-          <AnimateDigit value={Math.floor(timeLeft.calculation.second / 10)} />
-          <AnimateDigit value={timeLeft.calculation.second % 10} />
+        {/* Footer (Description) */}
+        <div className={`flex flex-col fixed bottom-8 text-center`}>
+          {/* <p>
+            {timeLeft.info.difference < 0
+              ? "Counting up since "
+              : "Counting down until "}
+            <b className="font-semibold">{eventName}</b>
+          </p> */}
+          <p>
+            <b className="font-medium">
+              {/* Get a 2-decimal percentage of time from M1-M6 completion. */}
+              {Math.floor(
+                (1 -
+                  (jarkyaoTime - new Date().getTime()) /
+                    (jarkyaoTime - enrolledTime)) *
+                  10000
+              ) / 100}
+              %
+            </b>
+            <span className="opacity-50"> (29/06/2020 – 20/02/2026)</span>
+          </p>
+          <p className="opacity-50">Febuary 20, 2026 @ 9:00 PM ICT</p>
         </div>
       </div>
-
-      {/* Footer (Description) */}
-      <div className={`flex flex-col fixed bottom-8 text-center`}>
-        {/* {timeLeft.info.difference < 0
-          ? "Counting up since "
-          : "Counting down until "}
-        <b className="font-semibold">{eventName}</b> */}
-        <p>
-          <b className="font-medium">
-            {Math.floor(
-              (1 -
-                (targetTime - new Date().getTime()) /
-                  (targetTime -
-                    new Date("2020-06-29T07:30:00+07:00").getTime())) *
-                10000
-            ) / 100}
-            %
-          </b>
-          <span className="opacity-50"> (29/06/2020 – 20/02/2026)</span>
-        </p>
-        <p className="opacity-50">Febuary 20, 2026 @ 9:00 PM ICT</p>
-      </div>
-    </div>
+    </>
   );
 };
 
